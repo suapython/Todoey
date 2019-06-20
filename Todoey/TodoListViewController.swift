64c4c4851fbf01f14c754149ab488aa5993a88c8
Francisco <j.f.morales.morera@gmail.comgit commit -m initial>
2019-06-21 00:24:46 +0200

    tableview create with four rows

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by jose francisco morales on 20/06/2019.
+//  Copyright © 2019 jose francisco morales. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    let itemArray = ["Find mike", "Buy Eggs", "Destroy dragons","fight against elfs" ]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+  //Mark -Tableview datasource methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+   
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+
+}
+
